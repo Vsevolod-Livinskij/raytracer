e398a180c802ce58ede2fe4034669fe4f371ce29
Vsevolod Livinskij <vsevolod.livinskij@frtk.ru>
2013-11-09 02:56:39 +0400

    There is some problem with random function, so it generates non-smooth shadows

---
+++ tracer.cpp
@@ -0,0 +1,24 @@
+/*
+ * tracer.cpp
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: vsevolod
+ */
+#include <stdlib.h>   // card > aek.ppm
+#include <stdio.h>
+#include <math.h>
+#include <algorithm>
+//#include "tracer_ispc.h"
+
+extern "C"
+void picture_creator (int width, int height, int depth, int out []);
+
+int main () {
+	printf("P6 512 512 255 "); // The PPM Header is issued
+	int width = 512, height = 512, depth = 3;
+	int *out = new int [width * height * depth];
+	picture_creator (width, height, depth, out);
+	for (int i = width * height * depth; i >= 0 ; i--) {
+		printf ("%c", out [i]);
+	}
+}
