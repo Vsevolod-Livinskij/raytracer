fe40d2830b4210266dc8282699b618ed077816de
Ilia Filippov <ili.filippov@gmail.com>
2014-01-13 14:44:06 +0400

    improving performance

--- tracer.cpp
+++ tracer.cpp
@@ -12,15 +12,15 @@
 
 extern "C"
 void picture_creator (int width, int height, int depth, int out []);
-extern "C"
-int programNum;
 
 int main () {
-	printf("P6 512 512 255 "); // The PPM Header is issued
-	int width = 512, height = 512, depth = 3;
-	int *out = new int [width * height * depth];
-	picture_creator (width, height, depth, out);
-	for (int i = width * height * depth - 1; i >= 0 ; i--) {
-		printf ("%c", out [i]);
-	}
+    printf("P6 512 512 255 "); // The PPM Header is issued
+    int width = 512, height = 512, depth = 3;
+    int *out = new int [width * height * depth];
+    picture_creator (width, height, depth, out);
+    for (int i = width * height; i >= 0 ; i--) {
+        printf ("%c", out [i + 2 * (width) * (height)]);
+        printf ("%c", out [i + 1 * (width) * (height)]);
+        printf ("%c", out [i + 0 * (width) * (height)]);
+    }
 }
